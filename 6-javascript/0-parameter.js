45171010e2ec2f68fca4838005f09a9a772d8077
Justin <justinsin717@gmail.com>
2018-07-13 11:53:45 -0700

    a

--- 6-javascript/0-parameter.js
+++
@@ -1,4 +0,0 @@
-// Store a value passed by the user to the script in the variable my_parameter. Note it takes the second element which corresponds to the first parameter. Third element would correspond to second parameter
-function my_parameter([PARAMETER]) {
-  console.log(Here is the first passed parameter: [PARAMETER])
-}
---
+++ 6-javascript_continued/0-parameter.js
@@ -0,0 +1,8 @@
+let my_parameter = process.argv[2];{
+console.log("Here is the first passed parameter: " + my_parameter)
+if (my_parameter > 5){
+    console.log("The value is greater than 5")
+}else{
+    console.log("The value is equal to 5 or lower")
+}
+}
